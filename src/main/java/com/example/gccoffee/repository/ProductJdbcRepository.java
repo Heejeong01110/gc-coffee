be8425b7bb922703962b5f90c7d3f0a7782aa612
Heejeong01110 <123rkdrkd@naver.com>
2021-09-24 18:01:45 +0900

    feat: product 정보 수정 구현

--- src/main/java/com/example/gccoffee/repository/ProductJdbcRepository.java
+++ src/main/java/com/example/gccoffee/repository/ProductJdbcRepository.java
@@ -38,7 +38,13 @@ public class ProductJdbcRepository implements ProductRepository{
 
     @Override
     public Product update(Product product) {
-        return null;
+        int update = jdbcTemplate.update("UPDATE products SET product_name = :productName, category = :category, price = :price, description = :description, created_at = :createdAt, updated_at = :updatedAt " +
+                        "WHERE product_id = UNHEX(REPLACE(:productId,'-',''))",
+                toParamMap(product));
+        if(update!=1){
+            throw new RuntimeException("Nothing was updated");
+        }
+        return product;
     }
 
     @Override
@@ -74,7 +80,7 @@ public class ProductJdbcRepository implements ProductRepository{
 
     @Override
     public void deleteAll() {
-
+        jdbcTemplate.update("DELETE FROM products",Collections.emptyMap());
     }
 
     private static final RowMapper<Product> productRowMapper = (resultSet, i) -> {
--- src/test/java/com/example/gccoffee/repository/ProductJdbcRepositoryTest.java
+++ src/test/java/com/example/gccoffee/repository/ProductJdbcRepositoryTest.java
@@ -3,15 +3,13 @@ package com.example.gccoffee.repository;
 import com.example.gccoffee.model.Category;
 import com.example.gccoffee.model.Product;
 import com.wix.mysql.EmbeddedMysql;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import static com.wix.mysql.ScriptResolver.classPathScript;
@@ -46,6 +44,11 @@ class ProductJdbcRepositoryTest {
         embeddedMysql.stop();
     }
 
+    @AfterEach
+    void cleanDB(){
+        repository.deleteAll();
+    }
+
     @Autowired
     ProductRepository repository;
 
@@ -61,5 +64,85 @@ class ProductJdbcRepositoryTest {
         //then
         List<Product> all = repository.findAll();
         assertThat(all.isEmpty(),is(false));
+
+    }
+
+    @Test
+    @DisplayName("상품을 id로 조회할 수 있다.")
+    public void testFindById() {
+        //given
+        UUID productId = UUID.randomUUID();
+        Product newProduct = new Product(productId,"new-product", Category.COFFEE_BEAN_PACKAGE,10000);
+        repository.insert(newProduct);
+
+        //when
+        Optional<Product> findProduct = repository.findById(productId);
+
+        //then
+        assertThat(findProduct.get(),samePropertyValuesAs(newProduct));
+    }
+
+    @Test
+    @DisplayName("상품을 이름으로 조회할 수 있다.")
+    public void testFindByName() {
+        //given
+        String productName = "new-product";
+        Product newProduct = new Product(UUID.randomUUID(), productName, Category.COFFEE_BEAN_PACKAGE,10000);
+        repository.insert(newProduct);
+
+        //when
+        Optional<Product> findProduct = repository.findByName(productName);
+
+        //then
+        assertThat(findProduct.get(),samePropertyValuesAs(newProduct));
+    }
+
+    @Test
+    @DisplayName("상품을 카테고리별로 조회할 수 있다.")
+    public void testFindByCategory() {
+        //given
+        Product newProduct1 = new Product(UUID.randomUUID(), "new-product1", Category.COFFEE_BEAN_PACKAGE,10000);
+        Product newProduct2 = new Product(UUID.randomUUID(), "new-product2", Category.COFFEE_BEAN_PACKAGE,20000);
+        repository.insert(newProduct1);
+        repository.insert(newProduct2);
+
+        //when
+        List<Product> products = repository.findByCategory(Category.COFFEE_BEAN_PACKAGE);
+
+        //then
+        assertThat(products,hasSize(2));
+    }
+
+    @Test
+    @DisplayName("상품을 전체 삭제한다.")
+    public void testDeleteAll() {
+        //given
+        Product newProduct1 = new Product(UUID.randomUUID(), "new-product1", Category.COFFEE_BEAN_PACKAGE,10000);
+        Product newProduct2 = new Product(UUID.randomUUID(), "new-product2", Category.COFFEE_BEAN_PACKAGE,20000);
+        repository.insert(newProduct1);
+        repository.insert(newProduct2);
+
+        //when
+        repository.deleteAll();
+
+        //then
+        List<Product> products = repository.findAll();
+        assertThat(products,hasSize(0));
+    }
+
+    @Test
+    @DisplayName("상품 정보를 수정할 수 있다.")
+    public void testUpdate() {
+        //given
+        Product newProduct = new Product(UUID.randomUUID(), "new-product", Category.COFFEE_BEAN_PACKAGE,10000);
+        repository.insert(newProduct);
+
+        //when
+        newProduct.setProductName("update-product");
+        repository.update(newProduct);
+
+        //then
+        Optional<Product> findProduct = repository.findById(newProduct.getProductId());
+        assertThat(findProduct.get(), samePropertyValuesAs(newProduct));
     }
 }
